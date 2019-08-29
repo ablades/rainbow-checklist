35e242a11e23cd8446a26e3f1a76d01d795f486a
ablades <audaris@gmail.com>
2019-08-28 19:15:23 -0700

    added mark_completed, select, and user_input functions

--- checklist.py
+++ checklist.py
@@ -17,6 +17,40 @@ def update(index, item):
 #Destroy
 def destroy(index):
     checklist.pop(index)
+
+#List
+def list_all_items():
+    index = 0
+    for list_item in checklist:
+        print("{} {}".format(index, list_item))
+        index += 1
+
+#Checkmark
+def mark_completed(index):
+    update(index, "√" + checklist[index])
+
+#Selects from user input
+def select(function_code):
+
+    if function_code == "C":
+        input_item = user_input("Input item:")
+        create(input_item)
+
+    elif function_code == "R":
+        item_index = user_input("Index Number?")
+        read(item_index)
+
+    elif function_code == "P":
+        list_all_items()
+
+    else:
+        print("Unknown Option")
+
+#Waits for user input
+def user_input(prompt):
+    user_input = input(prompt)
+    return user_input
+
 def test():
     create("purple sox")
     create("red cloak")
@@ -25,7 +59,13 @@ def test():
     update(0, "purple socks")
     destroy(1)
     print(read(0))
-    print(read(1))
+    list_all_items()
+    mark_completed(0)
+    list_all_items()
+    select("C")
+    list_all_items()
+    select("R")
+    list_all_items()
 
 test()
 
